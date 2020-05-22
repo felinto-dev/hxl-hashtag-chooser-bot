8fa65638a39dbf5f211653e4f366bf2640cf63f7
Felinto <emersong20.email@gmail.com>
2020-05-22 19:14:39 -0300

    Removed unnecessary code on src/commands/credits.js. Some typos solved.

--- src/commands/credits.js
+++ src/commands/credits.js
@@ -1,13 +1,8 @@
 export default (bot) => {
-  bot.command("start", async (ctx) => {
-    await ctx.reply(`👋👋👋 Welcome ${ctx.update.message.from.first_name}
-Answer the questions below to generate your HXL tag!`);
-    start_bot(ctx);
-  });
   bot.command("credits", async (ctx) => {
     ctx.reply(`👋👋👋 Hello ${ctx.update.message.from.first_name}!
 This bot was created by @felinto. This bot was created using an expert system (Artificial Intelligence) as part of a project from the Unidombosco faculty of the Data Science and Artificial Intelligence Course.
 
-Do you want such an expert system for you or your company? Hire me 😎 @felinto`);
+Do you want such an expert system like this for you or your company? Hire me 😎 @felinto`);
   });
 };
