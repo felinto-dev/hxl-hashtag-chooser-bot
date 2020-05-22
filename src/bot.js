418672c439bc6efd905680c969d180decdf612c2
Felinto <emersong20.email@gmail.com>
2020-05-22 06:03:00 -0300

    Added support for npm start. env-cmd was replaced for dotenv for improve Docker compatibility.

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 import Telegraf from "telegraf";
 import LocalSession from "telegraf-session-local";
 import rateLimit from "telegraf-ratelimit";
+require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TELEGRAM_TOKEN);
 
