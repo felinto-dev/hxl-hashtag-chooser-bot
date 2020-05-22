f0dfdb2e8608dfb9bca538bc192a31e778dbe411
Felinto <emersong20.email@gmail.com>
2020-05-22 13:59:39 -0300

    Just fixed a small typo.

--- src/commands/credits.js
+++ src/commands/credits.js
@@ -6,7 +6,7 @@ Answer the questions below to generate your HXL tag!`);
   });
   bot.command("credits", async (ctx) => {
     ctx.reply(`👋👋👋 Hello ${ctx.update.message.from.first_name}!
-This bot was created by @felinto. This bot was created using a expert system (Artificial Intelligence) as part of a project from the Unidombosco faculty of the Data Science and Artificial Intelligence Course.
+This bot was created by @felinto. This bot was created using an expert system (Artificial Intelligence) as part of a project from the Unidombosco faculty of the Data Science and Artificial Intelligence Course.
 
 Do you want such an expert system for you or your company? Hire me 😎 @felinto`);
   });
